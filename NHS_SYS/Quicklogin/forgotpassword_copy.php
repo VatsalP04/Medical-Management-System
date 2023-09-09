cee42e05203410fbd2d163cb762795513d666876
VatsalP04 <88625529+VatsalP04@users.noreply.github.com>
2023-09-08 21:42:11 -0400

    Update forgotpassword_copy.php

--- NHS_SYS/Quicklogin/forgotpassword_copy.php
+++ NHS_SYS/Quicklogin/forgotpassword_copy.php
@@ -19,8 +19,8 @@
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'vp9574962@gmail.com';                     //SMTP username
-            $mail->Password   = 'Vatsal04';                               //SMTP password
+            $mail->Username   = '#';                     //SMTP username
+            $mail->Password   = '#';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
